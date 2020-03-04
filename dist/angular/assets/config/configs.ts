1b03836233887fb312dfb4d58c0f3159e462462d
vatsal Shah <cevatsalshah@gmail.com>
2020-03-04 23:50:39 +0530

    dist

---
+++ dist/angular/assets/config/configs.ts
@@ -0,0 +1,6 @@
+export const config = {
+  // "apiUrl": "http://localhost:4000/",
+  "apiUrl": "http://swatch-007.com:4000/",
+  "assetUrl":"https://easocare.s3.ap-south-1.amazonaws.com/",
+  "port": "4070"
+}
